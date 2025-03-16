a0df91363a2f165a9a07446e21849de4c4e9db8d
Abhi Nair <88628074+hea7hen@users.noreply.github.com>
2025-03-16 18:03:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Abhishek_Nair/', // Set this to your repository name
+  base: '/Abhishek_Nair/',
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
   },
   plugins: [
@@ -16,7 +15,7 @@ export default defineConfig(({ mode }) => ({
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 }));
